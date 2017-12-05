e296d18eaafbb241a542db343453f3fb8a1d8930
svenhalvorson <svenedmail@gmail.com>
2017-12-05 11:22:31 -0700

    NSE for order_cols
    Changed order_cols to use NSE, have order_cols_q as quoted version

--- R/order_cols.R
+++ R/order_cols.R
@@ -1,106 +1,42 @@
 # We're here to write a function that reorders columns of a data frame
-# I would really like to do this with NSE as it would be cool to have it
-# look and interact with dplyr functions cleanly but I'm not sure I'm
-# at that level yet. Maybe we'll re-write it soon!
+# I'm improving on the original function now by adding NSE.
+# As a result, we have to change the arguments a bit
 
 # The arguments will be:
 #   df : a data frame to be re-ordered
-#   first : character vector of columns to be brought to the front
-#   last : character vector of columns to be brought to the back
+#   front: logical saying, whether to add the specified colomns to the front or back of df
 
-order_cols <- function(df, first = NA, last = NA){
+order_cols <- function(df, ..., front = TRUE){
 
   library("dplyr")
+
   # SAFEGAURDS --------------------------------------------------------------
 
   # Checks I'm thinking about right now:
   #  1. df should be a data frame
-  #  2. at least one of c(first, last) should not be NA
-  #  3. elements of first and last should be in colnames(df)
-  #  4. first and last should not overlap
-
-  # I think these functions will help. We want to allow for either first/last to be
-  # character vector or single NA
-  simple_NA = function(x){
-    if(length(x) != 1){
-      return(FALSE)
-    }
-    if(is.atomic(x) & is.na(x)){
-      return(TRUE)
-    }
-    else{
-      FALSE
-    }
-  }
-  chk <- function(x){
-    simple_NA(x) | (is.atomic(x) & is.character(x) & length(x) > 0)
-  }
-
-  if(!"data.frame" %in% class(df)){
+  if(!is.data.frame(df)){
     stop("df must be a data.frame")
   }
-
-  if(!suppressWarnings(chk(first)) | !suppressWarnings(chk(last))){
-    stop("First and last must be character vectors or NA")
+  #  2. elements of first and last should be in colnames(df)
+  cols <- as.character(substitute(list(...))[-1])
+  if(min(cols %in% colnames(df)) == 0){
+    missing_cols = cols[!cols %in% colnames(df)]
+    stop(paste("Columns not found in df:", paste(missing_cols, collapse = ", ")))
   }
 
-  if((length(first) == 1 & sum(is.na(first))>0) & (length(last) == 1 & sum(is.na(last))>0)){
-    stop("At least one of c(first, last) must be specified")
+  # WORK --------------------------------------------------------------------
+  # Now we have to get the leftover columns
+  leftovers = setdiff(colnames(df), cols)
+
+  # if every column was specified
+  if(length(leftovers) == 0){
+    return(df[cols])
   }
 
-  if((length(first)>1 & sum(is.na(first))>0) | (length(last)>1 & sum(is.na(last))>0)){
-    stop("first and last may not be named and contain NAs")
-  }
+  # if there are leftovers we treat them differently depending on front
+  if(front){cols = c(cols, leftovers)}
+  else {cols = c(leftovers, cols)}
+  df[cols]
 
-  if(!simple_NA(first) & mean(first %in% colnames(df)) < 1){
-    missing = setdiff(first,colnames(df))
-    stop(paste0("Values in first not found among colnames(df) : ",paste(missing, collapse = ", ")))
-  }
-
-  if(!simple_NA(last) & mean(last %in% colnames(df)) < 1){
-    missing = setdiff(last,colnames(df))
-    stop(paste0("Values in last not found among colnames(df) : ",paste(missing, collapse = ", ")))
-  }
-
-  if(length(setdiff(first, last))<length(first) | length(setdiff(last, first))<length(last)){
-
-    stop("first and last may not overlap")
-  }
-
-  # Separate df by columns --------------------------------------------------
-
-  # Now we'll just pick apart df by first last and middle and then glue together
-  extra = setdiff(setdiff(colnames(df),first),last)
-  # if all columns are specified by first and last
-  if(length(extra)==0){
-    if(simple_NA(first)){
-      df = df[last]
-      return(df)
-    }
-    if(simple_NA(last)){
-      df = df[first]
-      return(df)
-    }
-    else{
-      df = bind_cols(df[first],df[last])
-      return(df)
-    }
-  }
-
-  # If there are leftover columns
-  else if(simple_NA(first)){
-    df = bind_cols(df[extra], df[last])
-    return(df)
-  }
-  else if(simple_NA(last)){
-    df = bind_cols(df[first],df[extra])
-    return(df)
-  }
-  # if both are specified and there are leftover columns
-  else{
-
-    df = bind_cols(df[first], df[extra], df[last])
-    return(df)
-  }
 }
 
